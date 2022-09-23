438702e83427d6f2b2f3318e0cf8b98a0f20108b
montse murillo <87100892+mmurillo720@users.noreply.github.com>
2022-09-23 17:09:44 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+//Agregar las variables para guardar los datos
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+//colocar el cursor en automatico en el campo de texto
+
+guessField.focus();
+
+//aqui termina la seccion de variables
+
+//funciones
+// function checkGuess() {
+//   alert("Soy un marcador de posición");
+// }
+
+// checkGuess();
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Intentos anteriores: ";
+  }
+  guesses.textContent += userGuess + " ";
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "¡Felicidades! ¡Lo adivinaste!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "¡¡¡Fin del juego!!!";
+    setGameOver();
+  } else {
+    lastResult.textContent = "¡Incorrecto!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "¡El número es muy bajo!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "¡El número es muy grande!";
+    }
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+}
+
+//agregando un escucha de eventos
+guessSubmit.addEventListener("click", checkGuess);
+// Aquí estamos agregando un escucha de eventos al botón
+// guessSubmit. Este es un método toma dos valores de entrada
+// (llamados argumentos) — el tipo de evento que queremos
+// escuchar (en este caso, click) como una cadena, y el
+// código que queremos ejecutar
+// cuando ocurra el evento (en este caso la función checkGuess()).
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Iniciar nuevo juego";
+  document.body.append(resetButton);
+  resetButton.addEventListener("click", resetGame);
+}
+//funcion Reset Game
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
